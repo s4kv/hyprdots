c8feb955ef3437004e12b44cbc5c4efc24160cdb
s4kv <sibarguren21@gmail.com>
2025-07-30 13:47:37 -0400

    Changes to NeoVim config, Hyprland config, and other dotfiles. Important changes to monitor settings (including .Xresources), which changes the dpi and scaling of the display (especially for HiDPI displays).

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -594,6 +594,9 @@ require("lazy").setup({
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
 				-- local disable_filetypes = { c = true, cpp = true }
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
 				local disable_filetypes = {}
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
