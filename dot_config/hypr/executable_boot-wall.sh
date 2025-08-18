37c3cf79baece1aea001222c170f18d19410d870
s4kv <sibarguren21@gmail.com>
2025-08-18 10:26:09 -0400

    Changes in wallpaper configuration, other chnages in waybar and hyprland configuration files.

---
+++ dot_config/hypr/executable_boot-wall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+swww-daemon &
+sleep 2
+# this will also handle the start of waybar
+matugen image "/home/sakvi/Wallpaper/loop-wall/wallhavpurp.jpg"
+# swww img ~/Backgrounds/wallhavpurp.jpg
