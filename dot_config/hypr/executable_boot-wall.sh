cadb0b5c321cb7d743cfd69a0cea9ab7c7132522
s4kv <sibarguren21@gmail.com>
2025-09-16 19:37:47 -0400

    Breaking changes: Hyprland 0.51 upgrade Changed gesture system Changed to zathura theme so highlighted text is visible Temporary disable of plugins in hyprland due to instability (wait for future updates)

--- dot_config/hypr/executable_boot-wall.sh
+++ dot_config/hypr/executable_boot-wall.sh
@@ -2,5 +2,5 @@
 swww-daemon &
 sleep 2
 # this will also handle the start of waybar
-matugen image "/home/sakvi/Wallpaper/loop-wall/purple.jpg"
+matugen image "/home/sakvi/Wallpaper/loop-wall/abstract.jpg"
 # swww img ~/Backgrounds/wallhavpurp.jpg
