c35fb7f03e87c1d7cff17306ff7bbafc51e1ea89
s4kv <sibarguren21@gmail.com>
2025-10-18 13:35:09 -0400

    changes to neovim config

--- dot_config/nvim/lua/custom/plugins/debugprint.lua
+++ dot_config/nvim/lua/custom/plugins/debugprint.lua
@@ -3,33 +3,33 @@
 
 ---@type LazySpec
 return {
-	"https://github.com/andrewferrier/debugprint.nvim",
-	event = "VeryLazy",
+  'https://github.com/andrewferrier/debugprint.nvim',
+  event = 'VeryLazy',
 
-	---@type DebugprintGlobalOptions
-	opts = {
-		print_tag = "🤔 DEBUGPRINT",
-		keymaps = {
-			normal = {
-				plain_below = "g?p",
-				plain_above = "g?P",
-				variable_below = "g?v",
-				variable_above = "g?V",
-				variable_below_alwaysprompt = nil,
-				variable_above_alwaysprompt = nil,
-				textobj_below = "g?o",
-				textobj_above = "g?O",
-				toggle_comment_debug_prints = nil,
-				delete_debug_prints = "g?d",
-			},
-			visual = {
-				variable_below = "g?v",
-				variable_above = "g?V",
-			},
-		},
-		commands = {
-			toggle_comment_debug_prints = "ToggleCommentDebugPrints",
-			delete_debug_prints = "DeleteDebugPrints",
-		},
-	},
+  ---@type DebugprintGlobalOptions
+  opts = {
+    print_tag = '🤔 DEBUGPRINT',
+    keymaps = {
+      normal = {
+        plain_below = 'g?p',
+        plain_above = 'g?P',
+        variable_below = 'g?v',
+        variable_above = 'g?V',
+        variable_below_alwaysprompt = nil,
+        variable_above_alwaysprompt = nil,
+        textobj_below = 'g?o',
+        textobj_above = 'g?O',
+        toggle_comment_debug_prints = nil,
+        delete_debug_prints = 'g?d',
+      },
+      visual = {
+        variable_below = 'g?v',
+        variable_above = 'g?V',
+      },
+    },
+    commands = {
+      toggle_comment_debug_prints = 'ToggleCommentDebugPrints',
+      delete_debug_prints = 'DeleteDebugPrints',
+    },
+  },
 }
